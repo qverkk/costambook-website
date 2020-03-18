dd619ac64373bde346c9d5b81aac913562a1a0ef
qverkk <membersy@gmail.com>
2020-03-18 18:47:10 +0100

    Add sending posts to server

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -8,6 +8,8 @@ export default {
     return {
       description: "",
       choosenFile: null,
+      uploadOk: false,
+      uploadFailure: false,
       rules: {
         image: [
           value => !value || value.size < 2000000 || 'Image must be below 2MB!'
@@ -22,23 +24,51 @@ export default {
 
   },
   methods: {
+    resetWarrnings() {
+      this.uploadOk = false;
+      this.uploadFailure = false;
+    },
     createPost() {
-      // let data = JSON.stringify({
-      //   "description": this.description,
-      //   "image": this.choosenFile || ""
-      // })
-      // console.log(data);
-      // Axios.post("http://localhost:8090/posts", data, {
-      //     headers: {
-      //       "Content-Type": "application/json"
-      //     }
-      //   })
-      //   .then((response) => {
-      //     console.log(response);
-      //   })
-      //   .catch((response) => {
-      //     console.log(response);
-      //   })
+      this.resetWarrnings();
+
+      if (this.choosenFile != null) {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(this.choosenFile);
+        fileReader.addEventListener("load", () => {
+          this.uploadPost(fileReader.result);
+        });
+      } else {
+        this.uploadPost(null)
+      }
+    },
+    uploadPost(base64) {
+      if (base64 != null) {
+        this.sendPost(base64);
+      } else {
+        this.sendPost(null);
+      }
+    },
+    sendPost(blob) {
+      let wrapper = new Object();
+      wrapper.description = this.description;
+      wrapper.file = blob;
+
+      Axios({
+          url: "http://localhost:8090/posts",
+          data: wrapper,
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+        .then((response) => {
+          console.log(response);
+          this.uploadOk = true;
+        })
+        .catch((response) => {
+          console.log(response);
+          this.uploadFailure = true;
+        })
     }
   }
 }
